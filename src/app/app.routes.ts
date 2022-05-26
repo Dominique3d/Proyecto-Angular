f980bc98e8c3f525b2ed9cd32413428a3f74d6d2
Pablo <pabloasmdz3@hotmail.com>
2022-05-26 15:20:45 -0400

    contacto routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,16 +3,19 @@ import { HomeComponent } from "./components/home/home.component";
 import { CursoClaseBComponent } from "./components/curso-clase-b/curso-clase-b.component";
 import { LoginComponent } from "./components/login/login.component";
 import { FormPagoComponent } from "./components/form-pago/form-pago.component";
+import { ContactoComponent } from "./contacto/contacto.component";
 
 const APP_ROUTES: Routes = [
-    
+
     { path: 'home', component: HomeComponent,
         },
-    
+
     { path: 'curso-claseB', component: CursoClaseBComponent},
-    { path: 'login', component: LoginComponent},
+    { path: 'contacto', component: ContactoComponent},
+        { path: 'login', component: LoginComponent},
     { path: 'form-component', component: FormPagoComponent},
     { path: '**', pathMatch: 'full', redirectTo: 'home'}
+
 ];
 
 export const APP_ROUTING = RouterModule.forRoot(APP_ROUTES);
