f9e8e2892998f02605f8fd1a613a0708c3f62ebd
ClMacher <clmacher@gmail.com>
2022-08-20 23:20:29 -0400

    formbuilder-recepcionAEstudiante

--- src/app/perfiles/recepcion/datos-estudiantes/datos-estudiantes.component.ts
+++ src/app/perfiles/recepcion/datos-estudiantes/datos-estudiantes.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { EstudianteService } from 'src/app/services/estudiante.service';
+import { Estudiante } from 'src/app/interfaces/estudiante.interface';
 
 @Component({
   selector: 'app-datos-estudiantes',
@@ -8,9 +11,32 @@ import { Component, OnInit } from '@angular/core';
 export class DatosEstudiantesComponent implements OnInit {
   estudiantes: any[]=[];
 
-  constructor() { }
+  constructor(
+    private readonly fb: FormBuilder,
+    private readonly estudianteService: EstudianteService
+  ) { }
 
   ngOnInit(): void {
+  }
+
+  estudianteSelect: Estudiante | undefined;
+  updateEstudianteForm!: FormGroup;
+  updateEstudianteInputs: boolean[] = [];
+
+  toggleUpdateEstudianteInputs(i: number){
+    this.updateEstudianteInputs[i] = !this.updateEstudianteInputs[i];
+  }
+
+  initUpdateEstudianteForm(): FormGroup{
+    return(this.updateEstudianteForm = this.fb.group({
+      nombre: [this.estudianteSelect?.nombres, [Validators.required]],
+      primerApellido: [this.estudianteSelect?.primerApellido, [Validators.required]],
+      segundoApellido: [this.estudianteSelect?.segundoApellido, [Validators.required]],
+      email: [this.estudianteSelect?.email, [Validators.required]],
+      contrasena: [this.estudianteSelect?.contrasena, [Validators.required]],
+      telefono: [this.estudianteSelect?.telefono, [Validators.required]],
+      rut: [this.estudianteSelect?.rut, [Validators.required]]
+    }))
   }
 
 }
---
+++ src/app/services/estudiante.service.ts
@@ -0,0 +1,39 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { Estudiante } from '../interfaces/estudiante.interface';
+
+const base_url = environment.url_api;
+
+@Injectable({
+  providedIn: 'root'
+})
+export class EstudianteService {
+  constructor(private readonly http: HttpClient) { }
+
+  private readonly api_url = `${base_url}/estudiante`;
+
+  get token(): string {
+    return localStorage.getItem('token') || '';
+  }
+  getAllEstudiantes(): Observable<any> {
+    return this.http.get<any>(`${this.api_url}`);
+  }
+  getEstudianteById(id: string): Observable<any> {
+    return this.http.get<any>(`${this.api_url}/${id}`);
+  }
+  createEstudiante(estudiante: Estudiante): Observable<any> {
+    return this.http.post<any>(`${this.api_url}`, estudiante);
+  }
+  updateEstudiante(estudiante: Estudiante): Observable<any> {
+    return this.http.put<any>(
+      `${this.api_url}/${estudiante.id}`,
+      estudiante
+    );
+  }
+  deleteEstudiante(id: string): Observable<any> {
+    return this.http.delete<any>(`${this.api_url}/${id}`);
+  }
+}
+
