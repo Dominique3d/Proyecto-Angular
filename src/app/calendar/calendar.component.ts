604072679d93bde7093aa94bf30c81ae593cab83
Pablo <pabloasmdz3@hotmail.com>
2022-08-24 21:53:52 -0400

    limitar fecha

--- src/app/calendar/calendar.component.ts
+++ src/app/calendar/calendar.component.ts
@@ -1,13 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Injectable, OnInit } from '@angular/core';
 import { CalendarOptions }from '@fullcalendar/core';  // useful for typechecking
+import { now } from 'jquery';
 
 
-
+var today = new Date().toISOString().slice(0,10);
 @Component({
   selector: 'app-calendar',
   templateUrl: './calendar.component.html',
   styleUrls: ['./calendar.component.css']
 })
+
+
+@Injectable()
 export class CalendarComponent implements OnInit {
 
   calendarOptions: CalendarOptions = {
@@ -17,14 +21,18 @@ export class CalendarComponent implements OnInit {
       left: 'today',  //CAMBIAR EL BOTON TODAY a HOY
       center: 'title',
       right: 'prev,next',
-
     },
     buttonText: {
       today: 'Hoy'
     },
-
-    weekends: false
-    ,
+    weekends: false,
+    selectable: true,
+    validRange: function(nowDate) {
+      return {start: nowDate}
+    },
+    // select: function (start) {
+    //   var check = $.fullCalendar.formatDate(start, 'yyyy-MM-dd');
+    // },
     views: {
       dayGridMonth: { // name of view
         titleFormat: { year: 'numeric', month: 'long' }
@@ -56,3 +64,7 @@ export class CalendarComponent implements OnInit {
 
 
 }
+function moment() {
+  throw new Error('Function not implemented.');
+}
+
